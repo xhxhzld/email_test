f8e79433d8f60b175591b39608dc06488f627466
xhxhzld <xhxhzld@gmail.com>
2014-07-01 16:08:26 +0900

    실제메일

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,8 +4,18 @@ class EmailController < ApplicationController
 
   def email_send
     @email = params[:email]
+    @title = params[:title]
     @content = params[:content]
-   
+    
+    gmail = Gmail.connect("id","password")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
   end
 
+    gmail.logout
+  end
 end
